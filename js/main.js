67ffcb8d5e803f604f9e8bed16c80c49576025eb
Volody <Sydorenko.volodymyr1995@gmail.com>
2025-02-02 19:10:18 +0200

    intresting button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+// найшов бургер нажав
 document.querySelector('.burger').addEventListener('click',function()
 {
+   // якщо ctive є удаляєм якщо не має додаємо
 this.classList.toggle('active');
+// находит элемент с классом .nav и переключает у него класс open.
 document.querySelector('.nav').classList.toggle('open')
   }
 )
